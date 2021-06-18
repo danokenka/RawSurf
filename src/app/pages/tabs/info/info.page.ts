b37e7f5b0819c9a13276a5812aaa25f0549ce834
Daniel Okenka <danokenka@me.com>
2021-06-18 15:27:55 -0400

    add padding to top of title bars

--- src/app/pages/tabs/info/info.page.ts
+++ src/app/pages/tabs/info/info.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+
+const { Browser } = Plugins;
 
 @Component({
   selector: 'app-info',
@@ -10,6 +13,16 @@ export class InfoPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  async openBrowser() {
+  
+    // On iOS, for example, open the URL in SFSafariViewController (the in-app browser)
+    await Browser.open({ url: "https://www.raw.surf" });
+  }
+  async openBrowserInsta() {
+  
+    // On iOS, for example, open the URL in SFSafariViewController (the in-app browser)
+    await Browser.open({ url: "https://www.instagram.com/raw.surf" });
   }
 
 }
