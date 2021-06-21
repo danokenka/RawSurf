f8ca275b3d7f6a2cd9ce263d7621c14d3c4b3b42
Daniel Okenka <danokenka@me.com>
2021-06-20 22:45:34 -0400

    gutted most and fixed the bug for the thumbnail on book

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -3,9 +3,9 @@ import { AuthService } from '../../../services/auth.service';
  import { User } from '../../../models/user.model';
  import firebase from 'firebase/app';
 import 'firebase/auth';
-// import { Plugins } from '@capacitor/core';
+import { Plugins } from '@capacitor/core';
 
-// const { Browser } = Plugins;
+const { Browser } = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -21,54 +21,64 @@ export class HomePage {
     // this.myName = JSON.stringify(firebase.auth().currentUser.displayName);
      }
 
-    //  async openBrowserInsta() {
+     async openBrowserInsta() {
   
-    //   // On iOS, for example, open the URL in SFSafariViewController (the in-app browser)
-    //   await Browser.open({ url: "https://www.instagram.com/raw.surf" });
-    // }
-    // async openFB() {
+      // On iOS, for example, open the URL in SFSafariViewController (the in-app browser)
+      await Browser.open({ url: "https://www.instagram.com/raw.surf" });
+    }
+    async openFB() {
     
-    //   // On iOS, for example, open the URL in SFSafariViewController (the in-app browser)
-    //   await Browser.open({ url: "https://www.facebook.com/rawsurfingadventures/" });
-    // }
-    // async openInsta() {
+      // On iOS, for example, open the URL in SFSafariViewController (the in-app browser)
+      await Browser.open({ url: "https://www.facebook.com/rawsurfingadventures/" });
+    }
+    async openInsta() {
     
-    //   // On iOS, for example, open the URL in SFSafariViewController (the in-app browser)
-    //   await Browser.open({ url: "https://www.instagram.com/raw.surf" });
-    // }
-    // async openTwit() {
+      // On iOS, for example, open the URL in SFSafariViewController (the in-app browser)
+      await Browser.open({ url: "https://www.instagram.com/raw.surf" });
+    }
+    async openTwit() {
     
-    //   // On iOS, for example, open the URL in SFSafariViewController (the in-app browser)
-    //   await Browser.open({ url: "https://twitter.com/rawsurfingadv" });
-    // }
+      // On iOS, for example, open the URL in SFSafariViewController (the in-app browser)
+      await Browser.open({ url: "https://twitter.com/rawsurfingadv" });
+    }
 
-    // async openYoutu() {
+    async openYoutu() {
     
-    //   // On iOS, for example, open the URL in SFSafariViewController (the in-app browser)
-    //   await Browser.open({ url: "https://www.youtube.com/c/Rawsurfingadventures" });
-    // }
+      // On iOS, for example, open the URL in SFSafariViewController (the in-app browser)
+      await Browser.open({ url: "https://www.youtube.com/c/Rawsurfingadventures" });
+    }
+    async venmo() {
+    
+      // On iOS, for example, open the URL in SFSafariViewController (the in-app browser)
+      await Browser.open({ url: "https://venmo.com/RawSurf" });
+    }
 
-    //  socialMedia(type) {
-    //   switch (type) {
-    //     case 'FACEBOOK': {
+
+     socialMedia(type) {
+      switch (type) {
+        case 'FACEBOOK': {
           
-    //       this.openFB();
-    //       break;
-    //     }
-    //     case 'INSTAGRAM': {
-    //       this.openInsta();
-    //       break;
-    //     }
-    //     case 'TWITTER': {
-    //       this.openTwit();
-    //       break;
-    //     }
-    //     case 'YOUTUBE': {
-    //       this.openYoutu();
-    //       break;
-    //     }
-    //   }
-    // }
+          this.openFB();
+          break;
+        }
+        case 'INSTAGRAM': {
+          this.openInsta();
+          break;
+        }
+        case 'TWITTER': {
+          this.openTwit();
+          break;
+        }
+        case 'YOUTUBE': {
+          this.openYoutu();
+          break;
+        }
+        case 'VENMO': {
+          this.venmo();
+          break;
+        }
+      }
+    }
 
 //   getUserInfo() {
 //   console.log(firebase.auth().currentUser.email);
